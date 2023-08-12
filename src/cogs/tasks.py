e5911fe54a512e0d4fc6ff97a74b7e70924bbdc1
Eric Cimino <design.eric.cimino@gmail.com>
2023-08-12 09:51:06 -0500

    try making -1 -3

--- src/cogs/tasks.py
+++ src/cogs/tasks.py
@@ -80,7 +80,7 @@ class TasksCog(commands.Cog, name="Scheduled Tasks"): # type: ignore
             log.info("Starting updateFixtures")
 
             try:
-                fixtures = await self.bot.db.fetch("SELECT fixture_id FROM predictionsbot.fixtures WHERE event_date < now() + interval '5 hour' AND event_date > now() + interval '-1 hour' AND NOT scorable")
+                fixtures = await self.bot.db.fetch("SELECT fixture_id FROM predictionsbot.fixtures WHERE event_date < now() + interval '5 hour' AND event_date > now() + interval '-3 hour' AND NOT scorable")
                 # this is expensive if there a shitload of fixtures that happen to be within the window (the beginning of the FA cup for example)
                 # heuristic: for large numbers of fixtures, sort by event date and check newer events first
                 # if 10 or so don't update stop and log a warning saying to check the API for issues; usually signifies API slow or out of date
